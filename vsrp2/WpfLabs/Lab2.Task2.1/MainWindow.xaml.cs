24813d0354c990ba83fd6c57e88bd72f26ade999
Artsiom Kharkevich <artsiom_kharkevich@epam.com>
2022-12-26 02:20:52 +0300

    add lab2

--- vsrp2/WpfLabs/Lab2.Task2.1/MainWindow.xaml.cs
+++ vsrp2/WpfLabs/Lab2.Task2.1/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace Lab2.Task2._1
 {
@@ -20,9 +12,56 @@ namespace Lab2.Task2._1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer timer;
+        private readonly string figuresString = "M1 157C8.18884 69.3442 32.1596 32.4999 132 2C197.434 -1.92191 225.316 11.8068 273 40C306.8 83.5753 317.456 108.932 311 157C336.823 90.374 381.141 76.6902 395 75C408.859 73.3098 521.247 104.745 532 157C324.632 157 208.368 157 1 157Z";
+     
         public MainWindow()
         {
             InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += timer_randomAnimationTime;
+            timer.Start();
+        }
+
+        private void timer_randomAnimationTime(object sender, EventArgs e)
+        {
+            timer.Interval = TimeSpan.FromSeconds(new Random().Next(5, 15));
+
+            PlayAnim();
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Попали");
+        }
+
+        private void PlayAnim()
+        {
+            var pathGeometry = new PathGeometry
+            {
+                Figures = PathFigureCollection.Parse(figuresString)
+            };
+
+            var animation1 = new DoubleAnimationUsingPath
+            {
+                Source = PathAnimationSource.X,
+                Duration = new Duration(TimeSpan.FromSeconds(4)),
+                AccelerationRatio = 0.5,
+                PathGeometry = pathGeometry
+            };
+
+            var animation2 = new DoubleAnimationUsingPath
+            {
+                Source = PathAnimationSource.Y,
+                Duration = new Duration(TimeSpan.FromSeconds(4)),
+                AccelerationRatio = 0.5,
+                PathGeometry = pathGeometry
+            };
+
+            MyAnimatedTranslateTransform.BeginAnimation(TranslateTransform.XProperty, animation1);
+            MyAnimatedTranslateTransform.BeginAnimation(TranslateTransform.YProperty, animation2);
         }
     }
 }
