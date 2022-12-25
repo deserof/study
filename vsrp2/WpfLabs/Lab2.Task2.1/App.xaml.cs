af8ad7caee7713518b6b84e6869c3d9295c9bfb8
Artsiom Kharkevich <artsiom_kharkevich@epam.com>
2022-12-26 01:46:46 +0300

    add project for lab2 task2 1

--- vsrp2/WpfLabs/Lab2.Task2.1/App.xaml.cs
+++ vsrp2/WpfLabs/Lab2.Task2.1/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Lab2.Task2._2
+namespace Lab2.Task2._1
 {
     /// <summary>
     /// Interaction logic for App.xaml
