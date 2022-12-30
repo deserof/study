80b36f44418e8cac0c9265492a4f2bac4a0526fc
Artsiom Kharkevich <artsiom_kharkevich@epam.com>
2022-12-30 16:22:36 +0300

    small fix

--- Lab/Lab/Controllers/HomeController.cs
+++ Lab/Lab/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Lab.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Lab.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
