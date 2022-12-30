3af40a6d67a79113850a8fcfee62396f0cf38b78
Artsiom Kharkevich <artsiom_kharkevich@epam.com>
2022-12-30 16:10:21 +0300

    add lab

---
+++ Lab/Lab/Controllers/TestController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Controllers
+{
+    [Authorize]
+    public class TestController : Controller
+    {
+        // GET: TestController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TestController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TestController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TestController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TestController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TestController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TestController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TestController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
